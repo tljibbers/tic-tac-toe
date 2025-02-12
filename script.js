59fbb3864119be148054729b8904e7c31f5a942c
tljibbers <aahijoh@gmail.com>
2025-02-11 22:48:06 -0800

    set up files

---
+++ script.js
@@ -0,0 +1,113 @@
+let grid = [['-','-','-'],['-','-','-'],['-','-','-']]
+let xStamp = 'x'
+let oStamp = 'o'
+let winner = false;
+let player1Turn = false;
+let player2Turn = false;
+
+function printGrid(grid)
+{
+    console.log(grid[0][0] + " " + grid[0][1] + " " + grid[0][2]);
+    console.log(grid[1][0] + " " + grid[1][1] + " " + grid[1][2]);
+    console.log(grid[2][0] + " " + grid[2][1] + " " + grid[2][2]);
+}
+function checkDiagonalsLeft(grid)
+{
+    if(grid[0][0] == grid[1][1] == grid[2][2])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function checkDiagonalsRight(grid)
+{
+    if(grid[2][0] == grid[1][1] == grid[0][2])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function checkRows(grid)
+{
+    if(grid[0][0] == grid[1][0] == grid[2][0] || grid[0][1] == grid[1][1] == grid[2][1] || grid[0][2] == grid[1][2] == grid[2][2])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+function checkColumns()
+{
+    if(grid[0][0] == grid[0][1] == grid[0][2] || grid[1][0] == grid[1][1] == grid[1][2] || grid[2][0] == grid[2][1] == grid[2][2])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function play(grid)
+{
+    let userInputX = prompt("Please enter a value between 0 and 2");
+    if(userInputX > 2)
+    {
+        console.log("Number can't be higher than 2")
+        userInputX = prompt("Please enter a value between 0 and 2");
+    }
+    let userInputY = prompt("Please enter a value between 0 and 2");
+    if(userInputY > 2)
+    {
+        console.log("Number can't be higher than 2")
+        userInputY = prompt("Please enter a value between 0 and 2");
+    }
+
+    if(player1Turn == true)
+    {
+        grid[userInputX][userInputY] = xStamp;
+        printGrid(grid);
+        checkColumns(grid);
+        checkDiagonalsLeft(grid);
+        checkDiagonalsRight(grid);
+        checkRows(grid);
+        player1Turn = false;
+    }
+    if(player2Turn == true)
+    {
+        grid[userInputX][userInputY] = oStamp;
+        printGrid(grid);
+        checkColumns(grid);
+        checkDiagonalsLeft(grid);
+        checkDiagonalsRight(grid);
+        checkRows(grid);
+        player2Turn = false;
+    }
+
+    
+}
+
+
+
+function gameLoop()
+{
+    player1Turn = true;
+    while(winner == false)
+    {
+        play(grid)
+    }
+}
+
+gameLoop()
